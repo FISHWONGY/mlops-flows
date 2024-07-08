52a2654fa5f9f3d853a438ba9df98a77b5b81fec
FISHWONGY <59711659+FISHWONGY@users.noreply.github.com>
2024-07-08 14:35:54 -0400

    Update settings.py

--- 03-mlflow-wine-streamlit/app/settings.py
+++ 03-mlflow-wine-streamlit/app/settings.py
@@ -8,7 +8,7 @@ secrets = GCPSecrets()
 class AppConfig(BaseSettings):
     MLFLOW_TRACKING_USERNAME: str = secrets.get_secret("mlflow_server_username")
     MLFLOW_TRACKING_PASSWORD: str = secrets.get_secret("mlflow_server_password")
-    MLFLOW_TRACKING_URI: str = secrets.get_secret("mlflow_tracking_uri")
+    MLFLOW_ENDPOINT: str = secrets.get_secret("mlflow_endpoint")
     CRED_STR: str = f"{MLFLOW_TRACKING_USERNAME}:{MLFLOW_TRACKING_PASSWORD}"
     CRED_BYTES: bytes = str.encode(CRED_STR)
     CREDENTIALS: str = base64.b64encode(CRED_BYTES).decode("utf-8")
